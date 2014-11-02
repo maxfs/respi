111c4c7ee64bff69ce3e4b446dadf15840d9c5bf
Max <max.suffel@googlemail.com>
2014-11-02 15:19:32 -0800

    added empty respi.ino as a starting point

---
+++ respi.ino
@@ -0,0 +1,13 @@
+/**
+ * Project: Respi - A Teensy 3.1 Arduino-powered respiration monitor.
+ * Author: Max-Ferdinand Suffel
+ * Creation Data: 11-02-2014
+ */
+
+void setup () {
+
+}
+
+void loop () {
+
+}
